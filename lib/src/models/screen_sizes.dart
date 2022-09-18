997a4372d0eabcd36a8141fdfa82e96bea71248a
liodali <lionelviperdali@gmail.com>
2022-09-18 20:47:05 +0200

    feat : add screen_responsive widget * create screen_sizes to manage multiple sizes screen

---
+++ lib/src/models/screen_sizes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class ScreenSizes {
+  final Size xsmall;
+  final Size small;
+  final Size medium;
+  final Size large;
+  final Size xlarge;
+
+  ScreenSizes({
+    required  this.xsmall,
+    required this.small,
+    required this.medium,
+    required this.large,
+    required this.xlarge,
+  });
+}
---
+++ lib/src/screen_responsive.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:widget_responsive/src/models/screen_sizes.dart';
+
+/// Responsive
+///
+/// Widget that will make sure the child will be update to the different sizes of the screen
+///
+/// this widget should be as parent for all application
+///
+/// set [sizes] will help [child] in [Responsive] to change depend on the screen size
+class ScreenResponsive extends StatefulWidget {
+  const ScreenResponsive({
+    super.key,
+    required this.child,
+    required this.sizes,
+  });
+
+  // this child should be you application
+  final Widget child;
+
+  // sizes used to be used in [Responsive] Widget to adapt it self with different screen sizes
+  final ScreenSizes sizes;
+
+  @override
+  State<ScreenResponsive> createState() => ScreenResponsiveState();
+
+  static ScreenSizes? of(BuildContext context, {bool nullOk = false}) {
+    final ScreenResponsiveState? result =
+        context.findRootAncestorStateOfType<ScreenResponsiveState>();
+    if (nullOk || result != null) return result?.widget.sizes;
+    throw FlutterError.fromParts(<DiagnosticsNode>[
+      ErrorSummary(
+          'ScreenResponsive.of() called with a context that does not contain an ScreenSizes.'),
+      ErrorDescription(
+          'No ScreenResponsive ancestor could be found starting from the context that was passed to ScreenResponsive.of().'),
+      context.describeElement('The context used was')
+    ]);
+  }
+}
+
+class ScreenResponsiveState extends State<ScreenResponsive> {
+  late List<Size> sizes;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
