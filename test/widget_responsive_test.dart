dd3b2d0ca6e2ddcf8bbebaf413852d13ae812d7e
liodali <lionelviperdali@gmail.com>
2022-09-19 23:38:25 +0200

    * add test for responsive configuration

--- test/widget_responsive_test.dart
+++ test/widget_responsive_test.dart
@@ -1,12 +1,28 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:widget_responsive/widget_responsive.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('test adaptive size', () {
+    ResponsiveConfiguration.init(
+      screenSize: const Size(400, 800),
+      sizes: ScreenSizes(
+        xsmall: const Size(240, 320),
+        small: const Size(320, 480),
+        medium: const Size(460, 640),
+        large: const Size(570, 720),
+        xlarge: const Size(620, 880),
+      ),
+    );
+    final responsive = ResponsiveConfiguration.instance;
+    int result = responsive.adaptiveValue(20, {
+      RSizes.large: 25,
+      RSizes.medium: 20,
+      RSizes.small: 12,
+      RSizes.xsmall: 6,
+    });
+    expect(result, 12);
+    expect(result != 20, true);
   });
 }
