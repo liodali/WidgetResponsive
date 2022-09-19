472e299a38a22550be8beb86833f6753e2e4f8c7
liodali <lionelviperdali@gmail.com>
2022-09-19 23:40:11 +0200

    add export files

--- lib/widget_responsive.dart
+++ lib/widget_responsive.dart
@@ -1,7 +1,5 @@
 library widget_responsive;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/models/screen_sizes.dart';
+export 'src/widget/responsive.dart';
+export 'src/screen_responsive.dart';
--- test/widget_responsive_test.dart
+++ test/widget_responsive_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:widget_responsive/src/models/responsive_config.dart';
 
 import 'package:widget_responsive/widget_responsive.dart';
 
