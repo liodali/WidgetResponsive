e99065b9e1a1cbdfe1b07f26206d29e25fb7328d
liodali <lionelviperdali@gmail.com>
2022-09-19 23:37:31 +0200

    * add responsive configuration to adaptive value

---
+++ lib/src/models/responsive_config.dart
@@ -0,0 +1,53 @@
+import 'dart:ui';
+
+import 'package:widget_responsive/src/models/screen_sizes.dart';
+
+enum RSizes {
+  xlarge,
+  large,
+  medium,
+  small,
+  xsmall,
+}
+
+class ResponsiveConfiguration {
+  ResponsiveConfiguration._({
+    required Size screenSize,
+    required ScreenSizes sizes,
+  })  : _currentScreenSize = screenSize,
+        _sizes = sizes;
+  static late final ResponsiveConfiguration instance;
+
+  Size _currentScreenSize;
+  ScreenSizes _sizes;
+
+  Size get screenSize => _currentScreenSize;
+  ScreenSizes get sizes => _sizes;
+
+  static init({
+    required Size screenSize,
+    required ScreenSizes sizes,
+  }) {
+    instance = ResponsiveConfiguration._(
+      screenSize: screenSize,
+      sizes: sizes,
+    );
+  }
+
+  int adaptiveValue(
+    int value,
+    Map<RSizes, int> values,
+  ) {
+    int v = value;
+    final adaptSize = values.entries.firstWhere((element) {
+      final size = _sizes.sizes.firstWhere(
+        (s) =>
+            _currentScreenSize.width >= s.width &&
+            _currentScreenSize.height >= s.height,
+      );
+      final index = _sizes.sizes.indexOf(size);
+      return RSizes.values[index] == element.key;
+    });
+    return adaptSize.value;
+  }
+}
