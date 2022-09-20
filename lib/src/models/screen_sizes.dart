6f69a5d9a32d546673cf3eee2d36647dfc92f2c3
liodali <lionelviperdali@gmail.com>
2022-09-20 19:22:03 +0200

    add method return screen sizes in list in ScreenSizes

--- lib/src/models/screen_sizes.dart
+++ lib/src/models/screen_sizes.dart
@@ -8,10 +8,18 @@ class ScreenSizes {
   final Size xlarge;
 
   ScreenSizes({
-    required  this.xsmall,
+    required this.xsmall,
     required this.small,
     required this.medium,
     required this.large,
     required this.xlarge,
   });
+
+  List<Size> get sizes => [
+        xlarge,
+        large,
+        medium,
+        small,
+        xsmall,
+      ];
 }
